b260528ac3befdc8e8a1d52a8d0738cee84c9743
SabbathHex <sh@93546bd8-f8c4-40e9-a27a-3bd09ba47648>
2020-10-21 13:26:14 -0700

    added setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,31 @@ from setuptools import setup
 
 setup(
     name='confluence_poster',
-    version='0.2',
+    version='1.0',
     packages=['confluence_poster'],
-    install_requires=[
-        "atlassian-python-api>=1.15.7vi"
-    ],
+    package_dir={'': 'tests'},
     url='https://github.com/SabbathHex/confluence_poster',
     license='MIT',
     author='SabbathHex',
-    author_email='sh@93546bd8-f8c4-40e9-a27a-3bd09ba47648',
-    description='Supplementary script for writing confluence wiki articles in vim'
+    author_email='',
+    include_package_data=True,
+    entry_points={"console_scripts": ["confluence_poster = confluence_poster"]},
+    install_requires=[
+        "atlassian-python-api>=1.17.6",
+        "typer>=0.3.2",
+        "toml"
+    ],
+    long_description=open("README.md").read(),
+    tests_require=[
+        "pytest",
+        "pytest-cov",
+        "Faker"
+    ],
+    description='Script that updates Confluence articles from files written locally',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License"
+    ]
 )
