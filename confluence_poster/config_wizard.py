d9fc0f26b98657f35a8cf2edf5f9857c46fab307
Vladimir Timofeenko <id@vtimofeenko.com>
2020-12-24 13:27:00 -0800

    added path-like navigation in config files

---
+++ confluence_poster/config_wizard.py
@@ -0,0 +1,112 @@
+import typer
+from pathlib import Path
+from functools import reduce
+import toml
+from typing import List, Dict, Union, Any, Tuple
+from tomlkit import document, parse, table, dumps
+from tomlkit.parser import TOMLDocument
+from tomlkit.items import Table
+from dataclasses import dataclass
+from .poster_config import Auth, Page
+from copy import deepcopy
+
+
+def _get_attribute_by_path(attribute_path: str, config: TOMLDocument) -> Any:
+    """Given attribute path like path1.path2.attribute return value of attribute, None if attribute is empty
+     or path does not exist"""
+    attribute_path = attribute_path.split('.')
+    try:
+        return reduce(lambda caret, key: caret.get(key, None), attribute_path, config)
+    except AttributeError:  # for cases when we try to call get() on None
+        return None
+
+
+def _get_filled_attributes(config: TOMLDocument) -> Tuple[str]:
+    """For a config like this:
+
+    foo = 'bar'
+    [table]
+    foo = 'bar'
+    [table.child]
+    foo = 'bar'
+
+    produces (foo, table.foo, table.child.foo)
+    """
+    def key_walk(d: dict, base_path: str = '') -> str:
+        """A generator that traverses into a TOMLDocument, keeping track through base_path"""
+        for key in d:
+            path = base_path
+            if type(d[key]) is Table:
+                path += key + '.'
+                yield from key_walk(d[key], base_path=path)
+            else:
+                yield path + key
+    result = tuple(key_walk(config))
+    return result
+
+
+def _create_or_update_attribute(attribute: str, config: TOMLDocument, value: str) -> TOMLDocument:
+    """Given attribute path path1.path2.attribute, do the following:
+
+    1. Traverse the document, creating tables for path1 and path2 if they do not exist. 'path2' would be nested in path1
+    2. Set the attribute attribute to the value
+
+    Returns a copy of config with updated value
+    """
+    *attribute_path, attribute_name = attribute.split('.')
+    _config = deepcopy(config)
+    caret = _config
+    for path_node in attribute_path:  # more clear than a reduce() call
+        next_node = caret.get(path_node)
+        if next_node is None:
+            caret[path_node] = table()
+        caret = caret.get(path_node)
+
+    caret[attribute_name] = value
+    # return _config  # TODO: ugly. Otherwise _config dictionary mutates into {'foz': 'baz'} state
+    return parse(dumps(_config))
+
+
+def config_dialog(filename: Path, attributes: List[str]) -> Union[None, bool]:
+    """Checks if filename exists and goes through the list of attributes asking the user for the values
+    """
+    new_config = document()
+    if file_exists := filename.exists():
+        typer.echo(f"File {filename} already exists.")
+        typer.echo("Current content:")
+        print((content := filename.read_text()))
+        if not (prompt_overwrite := typer.confirm(f"File {filename} exists. Overwrite?",
+                                                  default=False)):
+            return  # do not save this config file
+
+        new_config = parse(content)
+    else:
+        prompt_overwrite = False
+
+    # Process attributes list
+    for attr in attributes:
+        default_value = _get_attribute_by_path(attr, new_config)
+        message = f"Please provide a value for {attr}"
+        if default_value is not None:
+            if not prompt_overwrite:
+                continue  # next attribute
+            message += f". Current value is: {default_value}. Hit [Enter] to use the current value."
+            new_value = typer.prompt(text=message, default=default_value)
+        else:
+            new_value = typer.prompt(text=message)
+
+        _create_or_update_attribute(attribute=attr, config=new_config, value=new_value)
+
+    typer.echo(f"Config to be saved in {filename}:")
+    typer.echo(message=dumps(new_config))
+
+    save = typer.prompt("Would you like to save it?", default=True)
+    if save:
+        typer.echo(f"Saving config as {filename}")
+        filename.write_text(dumps(new_config))
+        return True
+    else:
+        return False
+
+
+
---
+++ tests/unit tests/test_config_wizard.py
@@ -0,0 +1,85 @@
+from tomlkit import dumps, parse
+# noinspection PyProtectedMember
+from confluence_poster.config_wizard import _create_or_update_attribute as create_update_attr
+# noinspection PyProtectedMember
+from confluence_poster.config_wizard import _get_attribute_by_path as get_attribute_by_path
+# noinspection PyProtectedMember
+from confluence_poster.config_wizard import _get_filled_attributes as get_filled_attributes
+import pytest
+from itertools import product
+
+document = """root_foo = 'root_bar'
+control_value = 'value'  # To make sure it's not touched
+[parent]
+parent_foo = 'parent_bar'  # String
+[parent.child]
+child_foo = 'child_bar'"""
+source_config = parse(document)
+
+
+@pytest.mark.parametrize('path',
+                         product(['root', 'parent', 'parent.child', 'parent.child.nonexistent_child'],
+                                 ['foo', 'foz']),
+                         ids=lambda tup: f'Look for {tup[1]} in {tup[0]}')
+def test_get_attribute_by_path(path):
+    *_, location = path[0].split('.')
+    attribute_name = path[1]
+    if attribute_name == 'foz' or location == 'nonexistent_child':
+        attribute_value = None
+    else:
+        attribute_value = f"{location}_bar"
+
+    attribute_name = f"{location}_" + attribute_name
+    if location == 'root':
+        location = None
+
+    if location:
+        attribute_path = f"{'.'.join(_ + [location])}.{attribute_name}"
+    else:
+        attribute_path = f"{attribute_name}"
+
+    assert get_attribute_by_path(attribute_path=attribute_path,
+                                 config=source_config) == attribute_value
+
+
+@pytest.mark.parametrize('mode', product([
+    ('create', 'node'), ('create', 'table'), ('update', 'node')],
+    ['root', 'parent', 'child']),
+    ids=lambda tup: f'{tup[0][0]} {tup[0][1]} in {tup[1]} table'
+)
+def test_create_or_update_attribute(mode):
+    (action, obj), where = mode
+    value = 'baz'
+    if action == 'update':
+        attribute_path = f'{where}_foo'
+    else:
+        attribute_path = 'foz'
+
+    if obj == 'table':
+        attribute_path = 'new_table.' + attribute_path
+
+    if where == 'parent':
+        attribute_path = 'parent.' + attribute_path
+    elif where == 'child':
+        attribute_path = 'parent.child.' + attribute_path
+
+    updated_config = create_update_attr(attribute=attribute_path,
+                                        value=value,
+                                        config=source_config)
+    assert updated_config != source_config, "This function should not change the original content"
+    assert get_attribute_by_path(attribute_path=attribute_path, config=updated_config) == value
+    assert updated_config['control_value'] == 'value'
+    # Test that the attribute is actually added and found
+    # Maybe move this to a separate test? Probably dup?
+    if action == 'update':
+        checked_attributes = get_filled_attributes(source_config)
+    else:
+        checked_attributes = get_filled_attributes(source_config) + (attribute_path,)
+
+    assert set(checked_attributes) == set(get_filled_attributes(updated_config))
+
+
+def test_get_filled_attributes():
+    assert get_filled_attributes(source_config) == ('root_foo', 'control_value',
+                                                    'parent.parent_foo',
+                                                    'parent.child.child_foo')
