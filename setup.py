a2001d562cdceb0cfb5eb542a551b0dc8b40b93e
Vladimir Timofeenko <id@vtimofeenko.com>
2020-12-30 11:34:41 -0800

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name='confluence_poster',
     version='1.1.0',
-    url='https://github.com/SabbathHex/confluence_poster',
+    description='Script that updates Confluence articles from files written locally',
+    long_description=(here / "README.md").read_text(encoding='utf-8'),
+    long_description_content_type="text/markdown",
+    url='https://github.com/VTimofeenko/confluence_poster',
     license='MIT',
     author='Vladimir Timofeenko',
     author_email='confluence.poster.maintain@vtimofeenko.com',
@@ -20,8 +26,7 @@ setup(
         "xdg>=5.0.1",
         "tomlkit==0.7.0"
     ],
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
+    python_requires='>=3.8, <4',
     tests_require=[
         "pytest",
         "pytest-cov",
@@ -33,12 +38,17 @@ setup(
             'typer-cli'
         ]
     },
-    description='Script that updates Confluence articles from files written locally',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Topic :: Utilities",
+        "Topic :: Text Processing",
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License"
-    ]
+    ],
+    keywords='confluence api vim',
+    project_urls={
+        'Bug Reports': "https://github.com/VTimofeenko/confluence_poster/issues",
+        'Source': 'https://github.com/VTimofeenko/confluence_poster/'
+    }
 )
