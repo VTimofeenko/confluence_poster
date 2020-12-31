65430931a4cb35bf2ac8e9a0d346bebdc91c50d3
Vladimir Timofeenko <id@vtimofeenko.com>
2020-12-30 18:31:37 -0800

    bump version to 1.2.0

--- confluence_poster/main.py
+++ confluence_poster/main.py
@@ -10,7 +10,7 @@ from atlassian.errors import ApiError
 from dataclasses import dataclass, field
 from requests.exceptions import ConnectionError
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 default_config_name = "config.toml"
 
 
